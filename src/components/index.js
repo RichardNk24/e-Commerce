090ce40cd014478036953d64c6601c70caf6014a
Richard Sobelwa Nkulu <64165035+RichardNk24@users.noreply.github.com>
2022-02-25 11:03:37 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as NavBar } from                                                                                               './Navbar/Navbar'
+export { default as NavBar } from                                                                                              './Navbar/Navbar'
 
 export {default as Products} from './products/Products'

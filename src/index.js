f0e783d832eaafa623c26721ea4f6c81e544b4c5
Richard Sobelwa Nkulu <64165035+RichardNk24@users.noreply.github.com>
2022-03-01 14:30:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import App from               './App';
+import App from            './App';
 
 ReactDom.render(<App />, document.getElementById('root'));
 
